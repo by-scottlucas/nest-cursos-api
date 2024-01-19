8499ecaccc0f25d458923e702d31870779075698
Lucas S <Lucasluke307@gmail.com>
2024-01-19 15:04:55 -0300

    Corrigindo DocumentBuilder

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
 
   const options = new DocumentBuilder()
-  .setTitle('Cursos API')
-  .setVersion('1.0.0')
-  .setDescription('API relacionada a cursos')
-  .build();
+    .setTitle('Nest API Cursos')
+    .setVersion('1.0.0')
+    .setDescription('API relacionada a cursos')
+    .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
